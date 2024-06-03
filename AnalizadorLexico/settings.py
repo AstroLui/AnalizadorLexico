7085d9cb8e46b807d32faf7d611dd2f81ee44fa6
Astrou Lui <luisdaniel16an@gmail.com>
2024-06-03 08:44:51 -0400

    fix: finxing up the somethings

--- AnalizadorLexico/settings.py
+++ AnalizadorLexico/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
+INSTALLED_APPS += ['django_codemirror6']
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -118,7 +120,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'AnalizadoLexico/static'),)
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
