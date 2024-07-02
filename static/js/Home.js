b569ffc8f3c30bb18b1715dd05623754c642306b
Astrou Lui <luisdaniel16an@gmail.com>
2024-07-02 13:58:54 -0400

    feat: adding the somethings

--- static/js/Home.js
+++ static/js/Home.js
@@ -27,16 +27,45 @@ function Compilar()
 function AddCodeSpace(data){
     var codeSpaceMirror = document.getElementById("codeSpaceMirror");
     codeSpaceMirror.innerHTML = ``
-    Object.entries(data).forEach(([key, value]) =>{
-        var code = document.createElement('div');
+    var iLine = 1;
+    var content = '';
+    var tooltip = '';
+    for(var i = 0; i < Object.entries(data).length; i++){
+        if(data[i]['0'] != "NEWLINE"){
+            content += data[i]['1'] + ''
+            if(i == Object.entries(data).length-1){
+                tooltip += data[i]['0']
+            }else{
+                tooltip += data[i]['0'] + ' - '
+            }
+        }else if(data[i]['0'] == "NEWLINE"){
+            newLine(codeSpaceMirror, tooltip, iLine, content);
+            iLine++;
+            newLineEmpty(codeSpaceMirror, iLine);
+            iLine++;
+        }
+    }
+    newLine(codeSpaceMirror, tooltip, iLine, content);
+    iLine++;
+}
 
+function newLine(nodeFather, tooltip, iLine, content){
+    var code = document.createElement('div');
         code.innerHTML = 
         `
-            <div class="tooltip tooltip-bottom" data-tip="${value['0']}">
-                <pre class="cursor-pointer" data-prefix=${key}>${value['1']}</pre>
+            <div class="tooltip tooltip-bottom" data-tip="${tooltip}">
+                <pre class="cursor-pointer" data-prefix=${iLine}>${content}</pre>
             </div>
-            
         `
-        codeSpaceMirror.appendChild(code);
-    })
+    nodeFather.appendChild(code); 
+}
+function newLineEmpty(nodeFather, iLine){
+    var code = document.createElement('div');
+        code.innerHTML = 
+        `
+            <div>
+                <pre class="cursor-pointer" data-prefix=${iLine}></pre>
+            </div>
+        `
+    nodeFather.appendChild(code); 
 }
